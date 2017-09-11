bfbc364f228f18271c113c28d81e4c193fcdcd66
金亮 <liang.jin@hand-china.com>
2017-09-11 20:05:38 +0800

    Exception 练习

---
+++ Exception/src/com/hand/Exception_01.java
@@ -0,0 +1,27 @@
+package com.hand;
+
+/**
+ * Created by jinliang on 2017/9/11.
+ * Throwable类下 Error Exception
+ */
+class ExceptionEntity{
+
+    public void div(int x,int y){
+        try {
+            System.out.print(x/y);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+}
+
+public class Exception_01 {
+    public static void main(String args[]){
+        ExceptionEntity entity = new ExceptionEntity();
+        entity.div(4,0);
+        System.out.println("over");
+
+    }
+}
