b3357a50ba463a548e7f82e221418849e55dbecf
金亮 <liang.jin@hand-china.com>
2017-09-11 21:15:50 +0800

    add Exception_finally

---
+++ Exception/src/com/hand/Exception_finally.java
@@ -0,0 +1,35 @@
+package com.hand;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * Created by jinliang on 2017/9/11.
+ */
+public class Exception_finally {
+    public static void main(String args[]){
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream("a.txt");
+
+        }catch (IOException ioe){
+            System.out.println(ioe.getMessage());
+            //return 强制返回 会继续执行finally
+            return ;
+            //exit 退出虚拟机 不会执行finally
+            //System.exit(1);
+        }finally {
+            //关闭磁盘文件
+            if(fis !=null){
+                try {
+                    fis.close();
+                }catch (IOException ioe){
+                    ioe.printStackTrace();
+                }
+
+            }
+            System.out.println("finally已经执行，回收资源");
+        }
+    }
+
+}
